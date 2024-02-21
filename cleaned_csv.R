bbe7bb11dc63f19b9ab73eb9e16df5e747f40258
panavk <159407013+panavk@users.noreply.github.com>
2024-02-20 18:31:36 -0800

    cleaned_csv
    I tried joining all 3 dfs. You can take a shot at it because it did not work or try analyzing the data with 2 dfs split up.

--- cleaned_csv.R
+++ cleaned_csv.R
@@ -1,5 +1,3 @@
-
-
 # Delete all variables in your workspace. This will make it easier to test your script.
 rm(list = ls())
 
@@ -8,35 +6,33 @@ library(dplyr)
 library(stringr)
 library(testthat)
 
-
-
-
 #import in Charging Station data for EVs from data.gov
 #https://data.openei.org/files/106/alt_fuel_stations%20%28Jul%2029%202021%29.csv
 #save in variable
-charge_station_df <- read.csv("https://data.openei.org/files/106/alt_fuel_stations%20%28Jul%2029%202021%29.csv")
-
-#saving the location of cities and their respective counties in Washington
+charging_stations_df <- read.csv("https://data.openei.org/files/106/alt_fuel_stations%20%28Jul%2029%202021%29.csv")
 county_city_conversion <- read.csv("https://data.wa.gov/api/views/g2kf-7usg/rows.csv?accessType=DOWNLOAD")
-
-#import in EV sales in Washington 
 ev_sales_washington <- read.csv("https://data.wa.gov/api/views/3d5d-sdqb/rows.csv?accessType=DOWNLOAD")
-
-#trimming dataframe to only include stations in Washington, and of those, only eletric.
-charge_station_df <- charge_station_df %>%
-  filter(State == "WA")%>%
-  filter(Fuel.Type.Code == "ELEC")%>%
-  select(Fuel.Type.Code,City,Open.Date,EV.On.Site.Renewable.Source,Restricted.Access)
-#trimming dataframe to vehicles licenced in Washington, 
-#and getting the most up to date data possible 
-ev_sales_washington <-ev_sales_washington%>%
-  filter(State == "WA")%>%
-  filter(Date =="December 31 2023")
   
+# Filter for only electric fuel type and stations in Washington
+refined_charging_stations_df <- charging_stations_df %>%
+  filter(Fuel.Type.Code == "ELEC" & State == "WA" & str_detect(Groups.With.Access.Code, "^Public")) %>%
+  select(Station.Name, City)
 
-#joining charging stations by city into counties
-charge_station_county <- left_join(charge_station_df,county_city_conversion, by=c("City"="CITY.NAME"))
+# Select city and county name
+county_city_conversion <- county_city_conversion %>%
+  select(COUNTY.NAME, CITY.NAME)
 
-#joining EV sales, and charging station locatons
-combined_df <- left_join(ev_sales_washington,charge_station_county,by=c("County"="COUNTY.NAME"))
-combined_df<- distinct(combined_df,.keep_all = TRUE)
+# Filter for only EVs in WA and select relevant data on EVs
+ev_sales_washington <- ev_sales_washington %>% 
+  filter(State == "WA") %>% 
+  select(County, Electric.Vehicle..EV..Total, Percent.Electric.Vehicles)
+
+# Join with the county-city conversion to get the county names for each city and remove any NA values
+refined_charging_stations_df <- refined_charging_stations_df %>%
+  left_join(county_city_conversion, by = c("City" = "CITY.NAME")) %>%
+  filter(!is.na(COUNTY.NAME))
+
+# Now, perform the aggregation to count the number of stations per county
+refined_charging_stations_df <- refined_charging_stations_df %>%
+  group_by(COUNTY.NAME) %>%
+  summarise(Num_EV_Stations = n(), .groups = 'drop')
