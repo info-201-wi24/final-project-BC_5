89aba5a1b3bf2d52169cc62f145ed4ec942ef007
George <George10lee@gmail.com>
2024-03-04 18:57:34 -0800

    added csv to server

--- final project starter/final project starter/server.R
+++ final project starter/final project starter/server.R
@@ -2,5 +2,5 @@
 server <- function(input, output){
   
   # TODO Make outputs based on the UI inputs here
-  
+  combined_df <- read.csv("https://raw.githubusercontent.com/info-201-wi24/final-project-BC_5/main/Charging.csv")  
 }
