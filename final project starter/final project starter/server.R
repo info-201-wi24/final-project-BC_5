c269b0f9235a05399ec8c9362b3292730de73b37
George <George10lee@gmail.com>
2024-03-06 04:30:45 -0800

    added viz_1_tab

--- final project starter/final project starter/server.R
+++ final project starter/final project starter/server.R
@@ -1,10 +1,58 @@
+library(dplyr)
+library(ggplot2)
+library(leaflet)
+library(stringr)
 library("plotly")
 
+charging_stations_df <- read.csv("https://data.openei.org/files/106/alt_fuel_stations%20%28Jul%2029%202021%29.csv")
+
+refined_charging_stations_df <- charging_stations_df %>%
+  filter(Fuel.Type.Code == "ELEC" & State == "WA" & str_detect(Groups.With.Access.Code, "^Public")) %>%
+  select(Station.Name, City,Longitude,Latitude,Open.Date)
+
+# change filter date based on slider input, giving more stations on map
+charging_stations_with_date <- refined_charging_stations_df %>%
+  mutate(true_date =as.Date(Open.Date))
+
+culm_stations <- charging_stations_with_date %>%
+  na.omit() %>%
+  group_by(true_date) %>%
+  summarise(total_stations = n()) %>%
+  mutate(total_stations = cumsum(total_stations))
+
+# TODO Make outputs based on the UI inputs here
+combined_df <- read.csv("https://raw.githubusercontent.com/info-201-wi24/final-project-BC_5/main/Charging.csv")
+
 server <- function(input, output, session){
-  
-  # TODO Make outputs based on the UI inputs here
-  combined_df <- read.csv("https://raw.githubusercontent.com/info-201-wi24/final-project-BC_5/main/Charging.csv") 
-  
+  # Tab 1.
+  output$viz_1_map <- renderLeaflet({
+    # Filter the data based on the selected date
+    filtered_df <- charging_stations_with_date %>%
+      filter(true_date <= input$date)
+    
+    palette_fn <- colorFactor(palette = "Paired", domain = filtered_df[["City"]])
+    
+    # Create Leaflet map
+    leaflet(data = filtered_df) %>%
+      addProviderTiles("CartoDB.Positron") %>%
+      setView(lng = -122.3321, lat = 47.6062, zoom = 5.5) %>%
+      addCircles(
+        lat = ~Latitude, # specify the column for `lat` as a formula
+        lng = ~Longitude, # specify the column for `lng` as a formula
+        stroke = FALSE, # remove border from each circle
+        radius = 50,
+        fillOpacity = 1,
+        label = ~Station.Name,
+        color = ~palette_fn(filtered_df[["City"]]) #Color stations by their city.
+      )
+  })
+  # ggplot(culm_stations)+
+  #   geom_smooth(
+  #     mapping = aes(
+  #       x = true_date,
+  #       y=total_stations
+  #     ))
+
   filtered_data <- reactive({
     select_counties <- input$county_checkbox
     combined_df %>%
--- final project starter/final project starter/ui.R
+++ final project starter/final project starter/ui.R
@@ -1,3 +1,6 @@
+library(shiny)
+library(leaflet)
+library(bslib)
 
 
 ## OVERVIEW TAB INFO
@@ -10,16 +13,21 @@ overview_tab <- tabPanel("Overview Tab Title",
 ## VIZ 1 TAB INFO
 
 viz_1_sidebar <- sidebarPanel(
-  h2("Options for graph"),
-  #TODO: Put inputs for modifying graph here
+  h2("Pick a Date:"),
+  sliderInput("date", "Select Date:",
+              min = as.Date("2008-01-01"), # Set minimum date
+              max = as.Date("2024-12-31"), # Set maximum date
+              value = as.Date("2015-10-23"), # Set initial date
+              step = 31 # Set step size (31 days)
+  )
 )
 
 viz_1_main_panel <- mainPanel(
-  h2("Vizualization 1 Title"),
-  # plotlyOutput(outputId = "your_viz_1_output_id")
+  h2("Map of Washington"),
+  leafletOutput(outputId = "viz_1_map")    
 )
 
-viz_1_tab <- tabPanel("Viz 1 tab title",
+viz_1_tab <- tabPanel("Charging Stations Mapped",
   sidebarLayout(
     viz_1_sidebar,
     viz_1_main_panel
@@ -75,8 +83,6 @@ conclusion_tab <- tabPanel("Conclusion Tab Title",
  h1("Some title"),
  p("some conclusions")
 )
-
-
 
 ui <- navbarPage("Example Project Title",
   overview_tab,
