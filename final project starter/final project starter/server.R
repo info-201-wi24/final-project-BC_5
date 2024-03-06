416ef79a835120431358daeabc286f83bcf1f558
George <George10lee@gmail.com>
2024-03-05 23:45:56 -0800

    working map

--- final project starter/final project starter/server.R
+++ final project starter/final project starter/server.R
@@ -1,6 +1,35 @@
+library(dplyr)
+library(ggplot2)
+library(leaflet)
+library(stringr)
 
-server <- function(input, output){
+charging_stations_df <- read.csv("https://data.openei.org/files/106/alt_fuel_stations%20%28Jul%2029%202021%29.csv")
+
+refined_charging_stations_df <- charging_stations_df %>%
+  filter(Fuel.Type.Code == "ELEC" & State == "WA" & str_detect(Groups.With.Access.Code, "^Public")) %>%
+  select(Station.Name, City,Longitude,Latitude,Open.Date)
+
+# change filter date based on slider input, giving more stations on map
+charging_stations_with_date <- refined_charging_stations_df %>%
+  mutate(true_date =as.Date(Open.Date))
+server <- function(input, output) {
   
-  # TODO Make outputs based on the UI inputs here
-  combined_df <- read.csv("https://raw.githubusercontent.com/info-201-wi24/final-project-BC_5/main/Charging.csv")  
+  output$viz_1_map <- renderLeaflet({
+    # Filter the data based on the selected date
+    filtered_df <- charging_stations_with_date %>%
+      filter(true_date <= input$date)
+    
+    # Create Leaflet map
+    leaflet(data = filtered_df) %>%
+      addProviderTiles("CartoDB.Positron") %>%
+      setView(lng = -122.3321, lat = 47.6062, zoom = 5.5) %>%
+      addCircles(
+        lat = ~Latitude, # specify the column for `lat` as a formula
+        lng = ~Longitude, # specify the column for `lng` as a formula
+        stroke = FALSE, # remove border from each circle
+        radius = 10,
+        fillOpacity = .5,
+        color = "Red"
+      )
+  })
 }
--- final project starter/final project starter/ui.R
+++ final project starter/final project starter/ui.R
@@ -1,3 +1,6 @@
+library(shiny)
+library(leaflet)
+library(bslib)
 
 
 ## OVERVIEW TAB INFO
@@ -10,73 +13,81 @@ overview_tab <- tabPanel("Overview Tab Title",
 ## VIZ 1 TAB INFO
 
 viz_1_sidebar <- sidebarPanel(
-  h2("Options for graph"),
-  #TODO: Put inputs for modifying graph here
+  theme = bs_theme(version = 4, bootswatch = "minty"),
+  h2("Pick a Date:"),
+  viz_1_sidebar <- sidebarPanel(
+    dateInput("date",
+              "Select Date:", 
+              value = "2022-01-01", 
+              format = "mm/dd/yyyy",
+              min =   "2015-01-01",
+              max = "2024-01-01")
+    )
 )
 
 viz_1_main_panel <- mainPanel(
-  h2("Vizualization 1 Title"),
-  # plotlyOutput(outputId = "your_viz_1_output_id")
+  h2("Map of Washington"),
+  leafletOutput(outputId = "viz_1_map")    
 )
 
-viz_1_tab <- tabPanel("Viz 1 tab title",
+viz_1_tab <- tabPanel("Charging Stations Mapped",
   sidebarLayout(
     viz_1_sidebar,
     viz_1_main_panel
   )
 )
-
-## VIZ 2 TAB INFO
-
-viz_2_sidebar <- sidebarPanel(
-  h2("Options for graph"),
-  #TODO: Put inputs for modifying graph here
-)
-
-viz_2_main_panel <- mainPanel(
-  h2("Vizualization 2 Title"),
-  # plotlyOutput(outputId = "your_viz_1_output_id")
-)
-
-viz_2_tab <- tabPanel("Viz 2 tab title",
-  sidebarLayout(
-    viz_2_sidebar,
-    viz_2_main_panel
-  )
-)
-
-## VIZ 3 TAB INFO
-
-viz_3_sidebar <- sidebarPanel(
-  h2("Options for graph"),
-  #TODO: Put inputs for modifying graph here
-)
-
-viz_3_main_panel <- mainPanel(
-  h2("Vizualization 3 Title"),
-  # plotlyOutput(outputId = "your_viz_1_output_id")
-)
-
-viz_3_tab <- tabPanel("Viz 3 tab title",
-  sidebarLayout(
-    viz_3_sidebar,
-    viz_3_main_panel
-  )
-)
-
-## CONCLUSIONS TAB INFO
-
-conclusion_tab <- tabPanel("Conclusion Tab Title",
- h1("Some title"),
- p("some conclusions")
-)
-
-
+# 
+# ## VIZ 2 TAB INFO
+# 
+# viz_2_sidebar <- sidebarPanel(
+#   h2("Options for graph"),
+#   #TODO: Put inputs for modifying graph here
+# )
+# 
+# viz_2_main_panel <- mainPanel(
+#   h2("Vizualization 2 Title"),
+#   # plotlyOutput(outputId = "your_viz_1_output_id")
+# )
+# 
+# viz_2_tab <- tabPanel("Viz 2 tab title",
+#   sidebarLayout(
+#     viz_2_sidebar,
+#     viz_2_main_panel
+#   )
+# )
+# 
+# ## VIZ 3 TAB INFO
+# 
+# viz_3_sidebar <- sidebarPanel(
+#   h2("Options for graph"),
+#   #TODO: Put inputs for modifying graph here
+# )
+# 
+# viz_3_main_panel <- mainPanel(
+#   h2("Vizualization 3 Title"),
+#   # plotlyOutput(outputId = "your_viz_1_output_id")
+# )
+# 
+# viz_3_tab <- tabPanel("Viz 3 tab title",
+#   sidebarLayout(
+#     viz_3_sidebar,
+#     viz_3_main_panel
+#   )
+# )
+# 
+# ## CONCLUSIONS TAB INFO
+# 
+# conclusion_tab <- tabPanel("Conclusion Tab Title",
+#  h1("Some title"),
+#  p("some conclusions")
+# )
+# 
+# 
 
 ui <- navbarPage("Example Project Title",
   overview_tab,
   viz_1_tab,
-  viz_2_tab,
-  viz_3_tab,
-  conclusion_tab
+  #viz_2_tab,
+  #viz_3_tab,
+  #conclusion_tab
 )
